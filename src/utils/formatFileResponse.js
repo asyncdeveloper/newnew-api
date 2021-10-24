67ce4832d0f286096b85e83f710fd7d771c944cf
asyncdeveloper <contact@asyncdeveloper.com>
2021-10-24 23:42:49 +0100

    Update fileUrl

--- src/utils/formatFileResponse.js
+++ src/utils/formatFileResponse.js
@@ -6,15 +6,14 @@ const HOST = `${process.env.APP_URL}:${process.env.APP_PORT}`;
 
 export default (file, isVideo) => {   
     const { filename, path} = file; 
+    const fileUrl = `${HOST}/${path}`;
 
     if (isVideo) {
-        return {
-            fileUrl: `${HOST}/${path}`,        
-        };  
+        return { fileUrl };  
     }
      
     return {
-        fileUrl: `${HOST}/${path}`,
+        fileUrl,
         thumbnailUrl: `${HOST}/public/thumbs/${filename}`,
     };  
 };
