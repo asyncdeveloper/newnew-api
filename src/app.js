9fcc9dec4200e205ccbb7f814fe95b0e42610e41
asyncdeveloper <contact@asyncdeveloper.com>
2021-10-25 01:48:16 +0100

    Add 404 page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import errors from "./middlewares/errors";
 import routes from "./routes/index";
 import path from "path";
+import { NotFound } from "./utils/errors";
 
 dotenv.config();
 
@@ -22,8 +23,13 @@ class App {
         this.express.use("/public", express.static(path.resolve("./public")));
         
         this.express.use("/", routes);        
+
+        // Handle 404 
+        this.express.use((req, res) => {
+            throw new NotFound("not_found");           
+        });
         
-        this.express.use(errors);
+        this.express.use(errors);        
     }
 
 }
